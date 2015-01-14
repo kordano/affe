e9189aa3ee0ef01ec4f7b722e9fa554c8ff58dfd
kordano <konrad.kuehne@rocketmail.com>
2015-01-14 15:48:11 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject affe "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2665"]
+                 [gg4clj "0.1.0"]
+                 [incanter "1.9.0"]
+                 [aprint "0.1.1"]]
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [lein-gorilla "0.3.4"]])
---
+++ test/affe/core_test.clj
@@ -0,0 +1,7 @@
+(ns affe.core-test
+  (:require [clojure.test :refer :all]
+            [affe.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
